1f49c126a67cc7a94e466ea8d34bba964660865e
Thomas Laukkanen <tumppu91@hotmail.com>
2021-01-25 09:26:59 +0100

    Added files from laptop

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <Navbar />
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss"></style>
+
+<script>
+import Navbar from '@/components/Navbar.vue'
+
+export default {
+  components: {
+    Navbar,
+  },
+}
+</script>
