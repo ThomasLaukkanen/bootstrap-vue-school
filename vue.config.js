606f9de73db0a9533202bc64f5d77fde99e1da7f
Thomas Laukkanen <tumppu91@hotmail.com>
2021-01-28 11:58:27 +0100

    la till ikonerna

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,10 @@ module.exports = {
   pwa: {
     manifestOptions: {
       background_color: 'hotpink',
+      short_name: 'GBG',
     },
     name: 'Göteborgarna',
     themeColor: 'blue',
+    msTileColor: 'blue',
   },
 }
