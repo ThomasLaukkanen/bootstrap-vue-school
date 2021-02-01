d0939d92efcd3c2009e30db341b24ff0adfd7bf3
Thomas Laukkanen <tumppu91@hotmail.com>
2021-02-01 11:06:52 +0100

    Footer is now at the bottom

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,27 @@
 <template>
   <div id="app">
-    <Navbar />
-    <router-view />
+    <div id="wrapper">
+      <Navbar />
+      <router-view />
+    </div>
     <Footer />
   </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+#app {
+  position: relative;
+  min-height: 100vh;
+}
+
+footer {
+  position: absolute;
+  bottom: 0;
+}
+#wrapper {
+  padding-bottom: 64px;
+}
+</style>
 
 <script>
 import Navbar from '@/components/Navbar.vue'
--- src/views/Hem.vue
+++ src/views/Hem.vue
@@ -15,3 +15,5 @@ export default {
   },
 }
 </script>
+
+<style lang="scss" scoped></style>
