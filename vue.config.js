8b708ff7bc77c2df1ff2b0bdc4e1b54155165482
Thomas Laukkanen <tumppu91@hotmail.com>
2021-02-01 13:43:09 +0100

    Added chainWebpack in vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  chainWebpack: (config) => {
+    config.plugin('html').tap((args) => {
+      args[0].title = 'Göteborgarna'
+      return args
+    })
+  },
   pwa: {
     manifestOptions: {
       background_color: '#007bbd',
@@ -14,14 +20,6 @@ module.exports = {
           },
           urlPattern:
             'https://polisen.se/api/events?locationname=G%C3%B6teborg',
-        },
-        {
-          handler: 'NetWorkFirst',
-          options: {
-            networkTimeoutSeconds: 5,
-          },
-          urlPattern:
-            'https://data.riksdagen.se/personlista/?iid=&fnamn=&enamn=&f_ar=&kn=&parti=&valkrets=G%C3%B6teborgs+kommun&rdlstatus=&org=&utformat=json&sort=parti&sortorder=asc&termlista=',
         },
       ],
     },
