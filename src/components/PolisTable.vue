68ff5f858dfc3ff8c678f2f72b2d59e7e723188a
Thomas Laukkanen <tumppu91@hotmail.com>
2021-05-16 23:18:07 +0200

    datefix table

--- src/components/PolisTable.vue
+++ src/components/PolisTable.vue
@@ -13,33 +13,34 @@
 </template>
 
 <script>
-import axios from 'axios'
-export default {
-  data() {
-    return {
-      items: [],
-      fields: [
-        {
-          key: 'datetime',
-          sortable: true,
-          label: 'Datum',
-        },
-        { key: 'summary', sortable: true, label: 'Summering' },
-        { key: 'url', sortable: true, label: 'Länk' },
-        {
-          key: 'type',
-          sortable: true,
-          label: 'Typ',
-        },
-      ],
+  import axios from 'axios'
+  export default {
+    data() {
+      return {
+        items: [],
+        fields: [
+          {
+            key: 'datetime',
+            sortable: true,
+            label: 'Datum',
+            formatter: (key) => key.slice(2, 16)
+          },
+          { key: 'summary', sortable: true, label: 'Summering' },
+          { key: 'url', sortable: true, label: 'Länk' },
+          {
+            key: 'type',
+            sortable: true,
+            label: 'Typ'
+          }
+        ]
+      }
+    },
+    created() {
+      axios
+        .get('https://polisen.se/api/events?locationname=G%C3%B6teborg')
+        .then((response) => (this.items = response.data))
     }
-  },
-  created() {
-    axios
-      .get('https://polisen.se/api/events?locationname=G%C3%B6teborg')
-      .then((response) => (this.items = response.data))
-  },
-}
+  }
 </script>
 
 <style lang="scss" scoped></style>
